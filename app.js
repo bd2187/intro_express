0bf94e70c9f95103208741c511eb2b3b6b07f929
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-02-05 20:37:29 -0800

    Set up View Engine middleware. Create views directory with index.ejs

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 var app = express();
 
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Body Parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,9 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Set Static Path (for static resources. i.e css)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    console.log('hello world');
-    res.send('Hello World!');
+app.get('/', function(req, res) {    
+    res.render('index');
 });
 
 const port = 3000;
