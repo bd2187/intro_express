6cf8111a01406a39a4684b791dc4e368fb191783
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-02-06 20:52:50 -0800

    Create POST route for 'users/add'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -14,6 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set Static Path (for static resources. i.e css)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Validator middleware
+app.use(expressValidator());
 
 var users = [
     {
@@ -36,18 +40,49 @@ var users = [
     },
 ];
 
-app.get('/', function(req, res) {
+var foo = users.map(function(obj) {
+    obj.first_name = obj.first_name[0].toUpperCase() + obj.first_name.slice(1).toLowerCase();
+    obj.last_name = obj.last_name[0].toUpperCase() + obj.last_name.slice(1).toLowerCase();
+    return obj;
+});
 
-    var foo = users.map(function(obj) {
-        obj.first_name = obj.first_name[0].toUpperCase() + obj.first_name.slice(1).toLowerCase();
-        obj.last_name = obj.last_name[0].toUpperCase() + obj.last_name.slice(1).toLowerCase();
-        return obj;
-    });
+app.get('/', function(req, res) {
 
     res.render('index', {
         title: 'Users',
         users: foo
     });
+});
+
+app.post('/users/add', function(req, res) {
+
+    req.checkBody('first_name', 'First name is required').notEmpty();
+    req.checkBody('last_name', 'Last name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+
+        res.render('index', {
+            title: 'Users',
+            users: foo,
+            errors
+        });
+
+    } else {
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+        };
+
+        console.log('Success!');
+    }
+
+
+
+    res.end();
 });
 
 const port = 3000;
